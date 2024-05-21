1cac091441eb7e84a9d93b00c0153de48a558024
SANG PHẠM HỮU <phamhuusang@vietbando.vn>
2024-05-21 11:27:22 +0700

    first

---
+++ Server/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Chat.Models;
+
+namespace Chat.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View("InsertUserName");
+        }
+        
+        [HttpPost]
+        public IActionResult Index(string username)
+        {
+            return View("Index", username);
+        }
+        
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Server/README.md
@@ -0,0 +1,6 @@
+# Websocket-ChatRoom
+Websocket with ASP.net Core
+
+http://gunnarpeipman.com/2017/03/aspnet-core-websocket-chat/
+
+Thanks you Gunnar!
